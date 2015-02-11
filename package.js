14e5c2e183636755f8afd9bd46b972cc93a4bcfd
Nick Wientge <nick@exygeninteractive.com>
2015-02-11 13:53:03 -0800

    publish 0.1.5

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: "meteoric:autoform-ionic",
   summary: "Ionic theme for Autoform",
-  version: "0.1.4",
+  version: "0.1.5",
   git: "https://github.com/meteoric/autoform-ionic.git"
 });
 
